9120342467d2f262d3c14a48a71a66c79e5dad88
silviooosilva <gasparsilvio7@gmail.com>
2022-10-19 13:24:04 +0100

    Melhorando a organização

---
+++ Provinces_Codes.py
@@ -0,0 +1,21 @@
+
+province_code = {
+    'Luanda':'LA', 
+    'Bengo':'BG', 
+    'Kwanza-Norte':'KN',
+    'Kwanza-Sul':'KS',
+    'Uíge':'UE',
+    'Zaíre':'ZR',
+    'Malanje':'MJ',
+    'Lunda-Sul':'LS',
+    'Lunda-Norte':'LN',
+    'Moxico':'MX',
+    'Kuando Kubango':'KK',
+    'Cunene':'CN',
+    'Huíla':'HL',
+    'Huambo':'HB',
+    'Benguela':'BL',
+    'Namibe':'NB',
+    'Bié':'BI',
+    'Cabinda':'CB'
+}
