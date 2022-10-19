994ebbb1d3051316f359e2cf36d3951201d83769
silviooosilva <gasparsilvio7@gmail.com>
2022-10-19 13:17:33 +0100

    Melhorando a organização

--- generators.py
+++ generators.py
@@ -2,29 +2,10 @@ import random
 import re
 
 from colorama import Fore
+from Provinces_Codes import *
 
 const = '00'
 
-province_code = {
-    'Luanda':'LA', 
-    'Bengo':'BG', 
-    'Kwanza-Norte':'KN',
-    'Kwanza-Sul':'KS',
-    'Uíge':'UE',
-    'Zaíre':'ZR',
-    'Malanje':'MJ',
-    'Lunda-Sul':'LS',
-    'Lunda-Norte':'LN',
-    'Moxico':'MX',
-    'Kuando Kubango':'KK',
-    'Cunene':'CN',
-    'Huíla':'HL',
-    'Huambo':'HB',
-    'Benguela':'BL',
-    'Namibe':'NB',
-    'Bié':'BI',
-    'Cabinda':'CB'
-}
 number_code = random.randint(1, 99)
 ID_code = random.randint(1000000, 9000000)
 
